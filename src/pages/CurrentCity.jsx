48cbb5f8799c5d4226768d10d5689806b5e16e38
Maverick <maverickseneris@gmail.com>
2025-04-21 06:56:07 +0800

    feat: use browser's location to fetch weather data

--- src/pages/CurrentCity.jsx
+++ src/pages/CurrentCity.jsx
@@ -1,20 +1,51 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import sunny from "../assets/weather-icons/02_clear.svg";
 import Card from "../components/Card";
 import hourly from "../seed-data/hourlyseed";
 
+import axios from "axios";
+
 function CurrentCity() {
+  const [cityWeatherData, setCityWeatherData] = useState({})
+  useEffect(() => { 
+    const oneCallApiUrl = import.meta.env.VITE_OPENWEATHER_ONECALL_API_URL
+    const oneCallApiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+    
+    console.log("url and keys:", oneCallApiKey, oneCallApiUrl)
+    navigator.geolocation.getCurrentPosition(
+       (position) => {
+         const { latitude, longitude } = position.coords;
+         console.log("Browser coordinates:", latitude, longitude);
+         // Next step: fetch weather using these coords
+         axios
+           .get(
+             `${oneCallApiUrl}lat=${latitude}&lon=${longitude}&appid=${oneCallApiKey}`
+           )
+           .then((response) => {
+             console.log(response.data); // response object
+           })
+           .catch((error) => {
+             console.error(error);
+           });
+       },
+       (error) => {
+         console.error("Location error:", error.message);
+       }
+     );
+  },[])
+
+
   return (
     <div className="flex flex-col items-center w-100 h-100 px-8 mt-16">
       {/* Current Forecast */}
       <span className="font-extrabold text-4xl my-2">Madrid</span>
       <p className="text-s">Chances of rain: 0%</p>
       <img src={sunny} alt="clear" className="my-6 w-50 pl-4" />
-
       <p className="text-5xl font-bold mb-8">31&deg;</p>
+
       {/* Hourly Forecast */}
       <Card>
-        <p className="font-medium text-sm">Today's Forcast</p>
+        <p className="text-sm font-semibold text-gray-300">Today's Forcast</p>
         <div className="flex gap-4 my-2 justify-around items-center w-[100%]">
           {hourly.map((data, index) => {
             return (
@@ -24,11 +55,15 @@ function CurrentCity() {
                   index === 1 ? "border-x border-gray-500" : ""
                 }`}
               >
-                <span className="font-semibold pb-2 text-gray-300">{data.time}</span>
+                <span className="font-semibold pb-2 text-gray-300">
+                  {data.time}
+                </span>
                 {data.weather === "sunny" && (
                   <img src={sunny} className="w-18 pl-1.5" />
                 )}
-                <span className="font-bold text-gray-300">{data.temperature}&deg;</span>
+                <span className="font-bold text-gray-300">
+                  {data.temperature}&deg;
+                </span>
               </div>
             );
           })}
