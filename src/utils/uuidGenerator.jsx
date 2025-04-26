3bdbdfb23b3f9dc016d7dc5b709a13e9b80eec3b
Maverick <maverickseneris@gmail.com>
2025-04-27 06:23:46 +0800

    feat: add new data fetch from weatherApi for city-list

--- src/utils/uuidGenerator.jsx
+++ src/utils/uuidGenerator.jsx
@@ -1,9 +1,5 @@
 function generateUUID() {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (char) => {
-    const r = (Math.random() * 16) | 0;
-    const v = char === "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
+  return Math.random().toString(36).substring(2, 7);
 }
 
 
