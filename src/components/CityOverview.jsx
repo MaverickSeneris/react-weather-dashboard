3d268b1d8c67ac18c37e63f1cdd54a8f0ece57f9
Maverick <maverickseneris@gmail.com>
2025-04-28 11:56:04 +0800

    feat: add navigate back to prev page button to city overview page

--- src/components/CityOverview.jsx
+++ src/components/CityOverview.jsx
@@ -6,13 +6,21 @@ import HourlyContainer from "./HourlyContainer";
 import { useLocation, useNavigate } from "react-router";
 import CurrentWeatherContainer from "./CurrentWeatherContainer";
 import iconMap from "../utils/weatherIconMapper";
+import { BiChevronLeft } from "react-icons/bi";
 
 function CityOverview({ children }) {
   const { state } = useLocation();
   const { currentWeatherInfo, cityName } = state || {};
+  const navigate = useNavigate(); // for going back to previous page
   console.log(currentWeatherInfo.weatherIcon);
   return (
-    <div className="flex flex-col items-center w-100 px-4 mt-10 pb-2">
+    <div className="flex flex-col items-center w-100 px-4 mt-4 pb-2">
+      <button
+              onClick={() => navigate(-1)}
+              className="self-start font-medium text-white hover:text-blue-400 mb-4"
+            >
+              <BiChevronLeft className="w-8 h-8 mr-1" />
+            </button>
       <CurrentCityContainer
         cityName={cityName}
         popValue={currentWeatherInfo.chanceOfRain}
