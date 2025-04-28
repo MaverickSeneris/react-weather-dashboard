fed701776e4580a84200b720adfc70ea18a3c09b
Maverick <maverickseneris@gmail.com>
2025-04-28 11:52:06 +0800

    fix: fix city overview current city icon render error

--- src/components/CityOverview.jsx
+++ src/components/CityOverview.jsx
@@ -5,17 +5,18 @@ import DailyContainer from "./DailyContainer";
 import HourlyContainer from "./HourlyContainer";
 import { useLocation, useNavigate } from "react-router";
 import CurrentWeatherContainer from "./CurrentWeatherContainer";
+import iconMap from "../utils/weatherIconMapper";
 
 function CityOverview({ children }) {
   const { state } = useLocation();
   const { currentWeatherInfo, cityName } = state || {};
-  console.log(currentWeatherInfo.dailyWeatherInfo);
+  console.log(currentWeatherInfo.weatherIcon);
   return (
     <div className="flex flex-col items-center w-100 px-4 mt-10 pb-2">
       <CurrentCityContainer
         cityName={cityName}
         popValue={currentWeatherInfo.chanceOfRain}
-        weatherIcon={currentWeatherInfo.weatherIcon}
+        weatherIcon={iconMap[currentWeatherInfo.weatherIcon]}
         tempValue={currentWeatherInfo.temperature}
       />
 
