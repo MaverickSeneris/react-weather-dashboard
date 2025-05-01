acf86266f298e6399d6055d1789ad773e1d07abf
Maverick <maverickseneris@gmail.com>
2025-05-01 18:30:42 +0800

    feat: settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Header from "../components/ui/Header";
 import PageContainer from "../components/ui/PageContainer";
-
-
+import WeatherSettings from "../components/WeatherSettings";
 
 function Settings() {
   return (
-    <PageContainer >
-      <Header title={"Settings"}/>
-      <h1>Settings page is under construction</h1>
+    <PageContainer>
+      <Header title={"Settings"} />
+      <div className="mt-8">
+        <WeatherSettings />
+      </div>
     </PageContainer>
   );
 }
